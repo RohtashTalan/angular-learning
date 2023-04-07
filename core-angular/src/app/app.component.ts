ebc13cff5a5531bbeefe188c8c094c8e8586f15c
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-04-07 07:15:37 +0530

    template urls

--- core-angular/src/app/app.component.ts
+++ core-angular/src/app/app.component.ts
@@ -3,11 +3,7 @@ import {Component} from "@angular/core"
 
 @Component({
     selector: "my-app",
-    template: `<h1>Hey Angular</h1>`,
-    styles: [`
-    h1{
-        background-color: #fff;
-    }
-    `]
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
 export class AppComponent{}
