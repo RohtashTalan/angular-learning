8654ebc3e15f04ea34c8c545536902c570b5ac23
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-04-10 08:20:34 +0530

    login function added in componets

--- tic-tac-toe/src/app/app.component.ts
+++ tic-tac-toe/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+
+  winMessage: string = '';
+  isCross = false;
+  itemArray: string[] = new Array(9).fill('empty');
+
+
+  checkIsWinner = () => {
+    let winnerLogin = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 5, 8],
+      [2, 5, 6]
+    ]
+
+
+    for (const condition of winnerLogin) {
+      let [a, b, c] = condition;
+      if (this.itemArray[a] === this.itemArray[b] && this.itemArray[a] === this.itemArray[c] &&
+        this.itemArray[a] !== 'empty') {
+        return this.itemArray[a]
+      }
+    }
+return false
+  }
+
+
+  
+  constructor(private toastr: ToastrService) { }
+
+  reloadGame = () => {
+    this.winMessage = '';
+    this.isCross = false;
+    this.itemArray = new Array(9).fill('empty')
+  }
 }
