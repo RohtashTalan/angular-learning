33525cdf51e7508e5ff607e1463d66013bcff6ba
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-04-22 08:21:36 +0530

    random user api handling

--- random-card/src/app/card/card.component.ts
+++ random-card/src/app/card/card.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import {
+  faEnvelope,
+  faMapMarkedAlt,
+  faPhone,
+  faDatabase
+} from "@fortawesome/free-solid-svg-icons";
+
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
 
+  @Input()
+  user:any;
+
+  faEnvelope = faEnvelope;
+  faMapMarkedAlt = faMapMarkedAlt
+  faPhone = faPhone;
+  faDatabase = faDatabase;
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
