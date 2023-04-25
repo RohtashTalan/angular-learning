8b1f5b0d03de0fb1217dcdb768b174ceca9efa6e
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-04-25 08:28:49 +0530

    environment

--- github-login-firebase/src/app/app.module.ts
+++ github-login-firebase/src/app/app.module.ts
@@ -1,6 +1,9 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+//Components imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
@@ -11,6 +14,28 @@ import { HomeComponent } from './pages/home/home.component';
 import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
+
+
+
+
+
+
+// form and httpclient
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http"
+
+
+
+// for toast
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
+import {ToastrModule} from "ngx-toastr";
+
+//firebase related imports
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth'
+
+
 
 @NgModule({
   declarations: [
@@ -26,7 +51,13 @@ import { SignupComponent } from './pages/signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
