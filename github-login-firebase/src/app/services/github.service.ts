b244713f09d49734bcfff88c444790a219b6a9a8
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-04-26 07:15:12 +0530

    services added

--- github-login-firebase/src/app/services/github.service.ts
+++ github-login-firebase/src/app/services/github.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+// https:api.github.com/users/hiteshcoudhary
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserDetails(userName:string){
+    return this.http.get(`https:api.github.com/users/${userName}`)
+  }
+
+
+  getRepos(repoUrl:string){
+    return this.http.get(repoUrl);
+  }
+
+
+
+
 }
